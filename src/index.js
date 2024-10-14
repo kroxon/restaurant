10da150334d8d6c9a7bf3229b112e6870c22c814
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-10-14 14:50:04 +0200

    Create homePage and menuPage
    Created 2 pages with images. Menu pages has to be changed to grid display in right way.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import { greeting } from "./greeting.js";
+import './styles.css';
 
-console.log(greeting);
+import initial from "./homePage.js";
+import loadMenu from './menuPage.js';
+
+initial();
+
+const homeBtn = document.querySelector('button[aria-label="Go to Home page"]');
+const menuBtn = document.querySelector('button[aria-label="Go to Menu page"]');
+const aboutBtn = document.querySelector('button[aria-label="Go to About page"]');
+homeBtn.addEventListener('click', () => initial())
+menuBtn.addEventListener('click', () => loadMenu())
